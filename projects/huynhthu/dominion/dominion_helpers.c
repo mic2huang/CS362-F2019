d4d7297a7d184f78adb5bf35c919d1840b8b822f
Michelle Huynh <michelle.thuong.huynh@ibm.com>
2019-11-01 16:44:46 -0700

    Modify unittest3.c + debug helpers

--- projects/huynhthu/dominion/dominion_helpers.c
+++ projects/huynhthu/dominion/dominion_helpers.c
@@ -155,11 +155,13 @@ int playAmbassador(int currentPlayer, int choice1, int choice2, struct gameState
     state->supplyCount[state->hand[currentPlayer][choice1]] += choice2;
     
     //each other player gains a copy of revealed card
+    int supplySubtract = 0
     for (i = 0; i < state->numPlayers; i++)
     {
         if (i != currentPlayer)
         {
             gainCard(state->hand[currentPlayer][choice1], state, 0, i);
+            supplySubtract++;
         }
     }
 
