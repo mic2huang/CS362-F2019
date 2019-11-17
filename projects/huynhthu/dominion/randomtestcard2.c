6854e840d70bdc8fbca27e892d0c2578fae8d76d
Michelle Huynh <michelle.thuong.huynh@ibm.com>
2019-11-17 12:55:03 -0800

    Modify randomtestcard2

--- projects/huynhthu/dominion/randomtestcard2.c
+++ projects/huynhthu/dominion/randomtestcard2.c
@@ -63,78 +63,79 @@ void testPlayMinion(int thisPlayer, int choice1, int choice2, struct gameState *
         {
             failed++;
         }
-        else if (choice1 == 0 && choice2 == 1 && testG->handCount[thisPlayer + 1] >= 5)
-        {
-            if (testG->coins != G->coins + xtraCoins)
-            {
-                coinsFailed++;
-                thisTestResult = 0;
-            }
-
-            if (testG->numActions != G->numActions + xtraActions)
-            {
-                numActionsFailed++;
-                thisTestResult = 0;
-            }
-
-            if (testG->handCount[thisPlayer + 1] != 4)
-            {
-                otherHandCountFailed++;
-                thisTestResult = 0;
-            }
-
-            if (thisTestResult)
-            {
-                passed++;
-            }
-            else
-            {
-                failed++;
-            }
-        }
     }
-
-    int main()
+    else if (choice1 == 0 && choice2 == 1 && testG->handCount[thisPlayer + 1] >= 5)
     {
-
-        int choice1 = 0, choice2 = 0, handpos = 0;
-        int seed = 1000;
-        int numPlayers = 2;
-        int thisPlayer = 0;
-        struct gameState G, testG;
-        int i;
-        int k[10] = {baron, ambassador, adventurer, embargo, village, minion, mine, cutpurse,
-                     sea_hag, tribute};
-
-        // initialize a game state and player cards
-        initializeGame(numPlayers, k, seed, &G);
-
-        srand(time(NULL));
-
-        printf("----------------- Testing function: %s ----------------\n", TESTFUNC);
-
-        int iterations = 10000;
-        for (i = 0; i < iterations; i++)
+        if (testG->coins != G->coins + xtraCoins)
         {
-            // copy the game state to a test case
-            memcpy(&testG, &G, sizeof(struct gameState));
-            // generate random choice
-            choice1 = rand() % 2;
-            choice2 = rand() % 2;
-
-            // generate other player's handCount
-            testG.handCount[thisPlayer + 1] = rand() % 10;
-
-            testPlayMinion(thisPlayer, choice1, choice2, &testG, &G, handpos);
+            coinsFailed++;
+            thisTestResult = 0;
         }
-        printf("# total Tests: %d\n", passed + failed);
-        printf("# Passed Tests: %d\n", passed);
-        printf("# Failed Tests: %d\n", failed);
-        printf("# handCountFailed: %i\n", handCountFailed);
-        printf("# coinsFailed: %i\n", coinsFailed);
-        printf("# numActionsFailed: %i\n", numActionsFailed);
-        printf("# otherHandCountFailed: %i\n", otherHandCountFailed);
-        printf("\n >>>>> SUCCESS: Testing complete %s <<<<<\n\n", TESTFUNC);
 
-        return 0;
+        if (testG->numActions != G->numActions + xtraActions)
+        {
+            numActionsFailed++;
+            thisTestResult = 0;
+        }
+
+        if (testG->handCount[thisPlayer + 1] != 4)
+        {
+            otherHandCountFailed++;
+            thisTestResult = 0;
+        }
+
+        if (thisTestResult)
+        {
+            passed++;
+        }
+        else
+        {
+            failed++;
+        }
     }
+}
+
+int main()
+{
+
+    int choice1 = 0, choice2 = 0, handpos = 0;
+    int seed = 1000;
+    int numPlayers = 2;
+    int thisPlayer = 0;
+    struct gameState G, testG;
+    int i;
+    int k[10] = {baron, ambassador, adventurer, embargo, village, minion, mine, cutpurse,
+                 sea_hag, tribute};
+
+    // initialize a game state and player cards
+    initializeGame(numPlayers, k, seed, &G);
+
+    srand(time(NULL));
+
+    printf("----------------- Testing function: %s ----------------\n", TESTFUNC);
+
+    int iterations = 10000;
+    for (i = 0; i < iterations; i++)
+    {
+        // copy the game state to a test case
+        memcpy(&testG, &G, sizeof(struct gameState));
+        // generate random choice
+        choice1 = rand() % 2;
+        choice2 = rand() % 2;
+
+        // generate other player's handCount
+        testG.handCount[thisPlayer + 1] = rand() % 10;
+
+        testPlayMinion(thisPlayer, choice1, choice2, &testG, &G, handpos);
+    }
+    printf("# total Tests: %d\n", passed + failed);
+    printf("# Passed Tests: %d\n", passed);
+    printf("# Failed Tests: %d\n", failed);
+    printf("# handCountFailed: %i\n", handCountFailed);
+    printf("# coinsFailed: %i\n", coinsFailed);
+    printf("# numActionsFailed: %i\n", numActionsFailed);
+    printf("# otherHandCountFailed: %i\n", otherHandCountFailed);
+    printf("\n >>>>> SUCCESS: Testing complete %s <<<<<\n\n", TESTFUNC);
+
+    return 0;
+}
