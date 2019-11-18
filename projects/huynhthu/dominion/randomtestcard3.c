9bedcad1e9547070770237e8276898d3efcc6c97
Michelle Huynh <michelle.thuong.huynh@ibm.com>
2019-11-17 19:37:04 -0800

    debug test 3

--- projects/huynhthu/dominion/randomtestcard3.c
+++ projects/huynhthu/dominion/randomtestcard3.c
@@ -183,7 +183,7 @@ int main()
 
     printf("----------------- Testing function: %s ----------------\n", TESTFUNC);
 
-    int iterations = 10;
+    int iterations = 1;
     for (int i = 0; i < iterations; i++)
     {
         // copy the game state to a test case
