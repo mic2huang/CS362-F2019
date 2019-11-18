3ed1589864c2f019e3bc96cf4774d4915407c5a4
Michelle Huynh <michelle.thuong.huynh@ibm.com>
2019-11-17 17:27:05 -0800

    debug test 3

--- projects/huynhthu/dominion/randomtestcard3.c
+++ projects/huynhthu/dominion/randomtestcard3.c
@@ -59,7 +59,7 @@ void testPlayTribute(int thisPlayer, int nextPlayer, struct gameState *testG, st
         {
             for (int i = 0; i < 2; i++)
             {
-                tributeRevealedCards[i] = testG->discard[nextPlayer][i];
+                tributeRevealedCards[i] = testG->deck[nextPlayer][testG->deckCount[nextPlayer] - (i + 1)];
                 printf("deckCount = 0, assign Revealed card loop\n");
             }
         }
